fe7bc8f8d3b0244734ee94456107f73a753c8280
31415us <pius.vondaeniken@epfl.ch>
2013-11-11 22:47:43 +0100

    should now be correct A* implementation, implemented after the pseudocode on wikipedia

--- AStar.hpp
+++ AStar.hpp
@@ -23,51 +23,60 @@ std::vector<T> aStar(
 {
     std::unordered_set<T,std::function<size_t(T)>,std::function<bool(T,T)> > visited(0,hash,compare);
     std::unordered_map<T,T,std::function<size_t(T)>,std::function<bool(T,T)> > parent(0,hash,compare);
-    std::unordered_map<T,double,std::function<size_t(T)>,std::function<bool(T,T)> > distance_travelled(0,hash,compare);
+    std::unordered_map<T,double,std::function<size_t(T)>,std::function<bool(T,T)> > g_score(0,hash,compare);
+    std::unordered_map<T,double,std::function<size_t(T)>,std::function<bool(T,T)> > f_score(0,hash,compare);
     PriorityQueue<T> queue(hash,compare);
-
-    visited.insert(start);
-    distance_travelled[start] = 0.0;
 
     std::vector<T> result;
 
-    T current = start;
-    while(! compare(current,goal))
+    g_score[start] = 0.0;
+    f_score[start] = g_score[start] + heuristic(start,goal);
+
+    queue.update(start,f_score[start]);
+
+
+    
+    while(!queue.isEmpty())
     {
-        std::cout << "currently processing:" << std::endl;
+        auto current = queue.poll();
+        visited.insert(current);
+
+        std::cout << "currently exploring:" << std::endl;
+
         std::cout << current << std::endl;
-        // get neighbours of current state and filter out states already visited
-        std::vector<T> neigh;
+
+        if(compare(current,goal))
+        {
+            // backtrack path
+            while(parent.find(current) != parent.end())
+            {
+                result.push_back(current);
+                current = parent[current];
+            }
+            std::reverse(result.begin(),result.end());
+
+            break;
+        }
+
         for(auto n : neighbours(current))
         {
-            if(visited.find(n) == visited.end())
+            double tentative_g_score = g_score[current] + edgeLength(current,n);
+            double tentative_f_score = tentative_g_score + heuristic(n,goal);
+
+            if(visited.find(n) != visited.end() && tentative_f_score >= f_score[n])
             {
-                neigh.push_back(n);
+                continue;
+            }
+            else
+            {
+                parent[n] = current;
+                g_score[n] = tentative_g_score;
+                f_score[n] = tentative_f_score;
+                queue.update(n,f_score[n]);
             }
         }
 
-
-        double current_dist = distance_travelled[current];
-        for(auto n : neigh)
-        {
-            double dist = current_dist + edgeLength(current,n);
-            queue.update(n,dist + heuristic(n,goal));
-            distance_travelled[n] = dist;
-            parent[n] = current;
-            visited.insert(n);
-        }
-
-        current = queue.poll();
     }
-
-    // backtrack path
-    while(parent.find(current) != parent.end())
-    {
-        result.push_back(current);
-        current = parent[current];
-    }
-
-    std::reverse(result.begin(),result.end());
 
     return result;
 }
