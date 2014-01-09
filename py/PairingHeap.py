b02d4b5a91683b0aadbd4f2cdedea6a1062eb61d
31415us <pius.vondaeniken@epfl.ch>
2014-01-09 01:05:41 +0100

    just add not yet implemented

---
+++ py/PairingHeap.py
@@ -0,0 +1,22 @@
+
+
+class PairingHeap(object):
+
+    def __init__(self):
+        pass
+
+    def __setitem__(self,item,priority):
+        pass
+
+    def __nonzero__(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def merge(self,other):
+        pass
+
